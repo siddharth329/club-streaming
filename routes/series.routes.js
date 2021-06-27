30e6806554d03a770a3f5f2b0f25e2953fe98beb
Siddharth Agrawal <agrawalsiddharth329@gmail.com>
2021-06-27 18:29:48 +0530

    Fix: Fixed the episode and series routes file to have their own parameter checking

--- routes/series.routes.js
+++ routes/series.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { episodeExistsFromParamId } = require('../utils/index');
+const { seriesExistsFromParamId } = require('../utils/index');
 const {
 	getAllSeries,
 	getSeries,
@@ -14,12 +14,12 @@ const router = express.Router();
 
 // OPEN TO ALL ROUTER
 router.get('/', validate('getAllSeries'), getAllSeries);
-router.get('/:id', episodeExistsFromParamId, validate('getSeries'), getSeries);
+router.get('/:id', seriesExistsFromParamId, validate('getSeries'), getSeries);
 
 // ADMIN ROUTES TO BE SECURED
 router.use(protected([ 'ADMIN' ]));
 router.post('/', validate('createSeries'), createSeries);
-router.patch('/:id', episodeExistsFromParamId, validate('updateEpisode'), updateSeries);
-router.delete('/:id', episodeExistsFromParamId, deleteSeries);
+router.patch('/:id', seriesExistsFromParamId, validate('updateEpisode'), updateSeries);
+router.delete('/:id', seriesExistsFromParamId, deleteSeries);
 
 module.exports = router;
